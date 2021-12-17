5fe909fadb1c90668b2e00cc93f7cefbe568d16f
Nunu2130 <gertrude.masipa@gmail.com>
2021-12-17 23:08:48 +0200

    Exam Final  Submission 17 Dec 2021

--- POM3/src/test/java/ExamTests/TC0SkipTest.java
+++ POM3/src/test/java/ExamTests/TC0SkipTest.java
@@ -9,61 +9,54 @@ import org.testng.annotations.Test;
 import examObj.ExamObj;
 import frameWorkClasses.BasePage;
 
-public class TC0SkipTest 
-{	
+public class TC0SkipTest {
 
-	ExamObj ex = new ExamObj(); 
-	BasePage bp =  new BasePage();
+	// 1.Skip test on Daddy deals if the brand does not have deals 
+	
+	ExamObj ex = new ExamObj();
+	BasePage bp = new BasePage();
+	
+	// Check for the Cookie button,Click on the it displayed
 
 	@BeforeTest
-	public void cookies() 
-	{
+	public void cookies() {
 		ex.clickGotIt();
-	}	
-	
-	
+	}
+
 	@Test
 	public void GIVEN_ICLickDailyDeals_WHEN_ItemDoesNotHaveDeals_THEN_SkipTestCase() throws InterruptedException
-	
-		//1 Select at least 1 brand i.e Acer or Addis etc
-		//2 Assert that there are more than 0 items displayed
-		//3 (bonus) if less than 1 item skip the test case	
-	
+
+	// 1 Select at least 1 brand i.e Acer or Addis etc
+	// 2 Assert that there are more than 0 items displayed
+	// 3 (bonus) if less than 1 item skip the test case
+
 	{
-			
-			//GIVEN
-				//I Click Daily Deals
-					//ex.clickGotIt();
-					Thread.sleep(1000);
-					ex.clickDailyDeals();
-					Thread.sleep(1000);
-												 
-		   //WHEN
-				//I Select a brand that doesn't have daily deals
-		
-				ex.captureBrand("gucci");
-				
-				String actBrandMSG = ex.BrandConfirmMsg();
-				String expectedBrandMSG = "No results found for ";
-					
-			
-			//THEN
-				//I skip the test
-		
-				if (!(actBrandMSG==expectedBrandMSG))
-				{
-					
-				throw new SkipException("Skipping the test case because brand is not found to have any daily deals" );	
-				}
-				
-				Thread.sleep(1000);
-				//bp.cleanup();
-			
-			}
-	
+
+		// GIVEN
+		// I Click Daily Deals
+		ex.clickDailyDeals();
+		Thread.sleep(1000);
+
+		// WHEN
+		// I Select a brand that doesn't have daily deals
+
+		ex.captureBrand("gucci");
+
+		String actBrandMSG = ex.BrandConfirmMsg();
+		String expectedBrandMSG = "No results found for ";
+
+		// THEN
+		// I skip the test
+
+		if (!(actBrandMSG == expectedBrandMSG)) {
+
+			throw new SkipException("Skipping the test case because brand is not found to have any daily deals");
+		}
+
+	}
+
 	@AfterTest
-	public void cleanup() 
-		{
-			bp.cleanup();
-		}	
+	public void cleanup() {
+		bp.cleanup();
+	}
 }
--- POM3/src/test/java/ExamTests/TC1AddToCart.java
+++ POM3/src/test/java/ExamTests/TC1AddToCart.java
@@ -1,6 +1,6 @@
 package ExamTests;
 
- import org.testng.Assert;
+import org.testng.Assert;
 
 import frameWorkClasses.BasePage;
 
@@ -9,61 +9,63 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import examObj.ExamObj;
-public class TC1AddToCart
-{
-	
-	ExamObj ex = new ExamObj(); 
-	BasePage bp =  new BasePage();
-	
+
+public class TC1AddToCart {
+
+	ExamObj ex = new ExamObj();
+	BasePage bp = new BasePage();
+
+	// Check for the Cookie button,Click on the it displayed
 	@BeforeTest
-	public void cookies() 
-	{
+	public void cookies() {
 		ex.clickGotIt();
-	}	
-	
-	
-	@Test	
+	}
+
+	@Test
 	public void GIVEN_ICLickFistItem_AND_IADDtoCart_THEN_IAssertItemAddedToCart() throws InterruptedException
-	
-	//4 Click the first item 
-	//5 Add it to the cart
-	//6 Assert that “Added to cart” appears at the top of the pop up window
-{
-		
-	//GIVEN
-		//I select first Item
-		ex.captureMainBrand("Maxwell & Williams");	
+
+	// 4 Click the first item
+	// 5 Add it to the cart
+	// 6 Assert that “Added to cart” appears at the top of the pop up window
+	{
+
+		// GIVEN
+		// I select first Item
+		// Search for brand
+		ex.captureMainBrand("Maxwell & Williams");
 		Thread.sleep(1000);
 		ex.clickSearchIcon();
 		Thread.sleep(1000);
-			 
-	//WHEN
-		//I add to Cart
+
+		// WHEN
+		// I add to Cart
+
 		ex.clickFirstItem();
 		Thread.sleep(1000);
 		ex.clickAddItemToCart();
 		Thread.sleep(1000);
+		// Confirm that the 'Added to Cart msg is displayed
 		ex.CartConfirmMsg();
+		Thread.sleep(1000);
+		// Close the sliding window after adding to cart
 		ex.clickSlider();
-					
-	//THEN
-		//I Assert that Item is added to cart  
+
+		// THEN
+		// I Assert that Item is added to cart
 		String successMsg = ex.CartConfirmMsg();
 		String expectedsuccessMsg = ("Added to cart");
 		System.out.println("");
+
+		// Assert that the "Add to Cart" Message is displayed after item is added to
+		// cart
 		Assert.assertEquals(successMsg, expectedsuccessMsg);
-		//String item = ex.identifyItem();
-		//System.out.println(item);
 		System.out.println("Selected item has been Added to Cart");
 		System.out.println("");
-	}	
-	
-	@AfterTest
-	public void cleanup() 
-		{
-			bp.cleanup();
-		}	
-}
-	
-	
+	}
 
+	// Close browser
+	@AfterTest
+	public void cleanup() {
+		bp.cleanup();
+	}
+}
